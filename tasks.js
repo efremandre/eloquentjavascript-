0ccb8cf27fed247d538be79eea0c4bc23a5eb80f
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-12-02 02:46:14 +0300

    first commit

---
+++ tasks.js
@@ -0,0 +1,14 @@
+import { chess, createTriangle, getFizzBuzz } from './chapter_1.js'
+
+console.log('Task #1')
+createTriangle()
+console.log(' ')
+
+console.log('Task #2')
+console.log(getFizzBuzz())
+console.log(' ')
+
+console.log('Task #3')
+console.log(chess(8))
+console.log(chess(7))
+console.log(' ')
